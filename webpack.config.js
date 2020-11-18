87bc7d2e0d5af93fe2ead6064f5eaa979d30ca0b
DecentRandom <misskiwi@decentrandom.com>
2020-11-18 14:47:46 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,28 @@ module.exports = {
     devServer: {
         port: 8081
     },
+    module: {
+        rules: [
+            {
+                test: /\.(gif|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "index.html",
